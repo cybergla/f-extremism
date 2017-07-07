41549a32a2701ca91b85f62bfc7486011085d39a
cybergla <tanaydeshmukh@gmail.com>
2017-07-07 14:47:40 +0400

    Initial Commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+      var url = tabs[0].url;
+      console.log(url);
+      var img = document.getElementById('spinner');
+      var content = document.getElementById('content')
+      img.setAttribute('hidden','true');
+      content.innerHTML = '<p>' + url + '</p>'
+  });
