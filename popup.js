945539591c1a3149aa7430c631db5e39277e2330
cybergla <tanaydeshmukh@gmail.com>
2017-07-08 11:19:01 +0400

    Added background page

--- popup.js
+++ popup.js
@@ -1,10 +1,17 @@
+var img = document.getElementById('spinner');
+var content = document.getElementById('content');
+
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
       var url = tabs[0].url;
       console.log(url);
-      
-      var img = document.getElementById('spinner');
-      var content = document.getElementById('content');
+      chrome.runtime.sendMessage({url:url, from_popup:true},null);
+  });
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) { 
+    console.log('Got message');
+    if (request.to_popup == true){
+      content.innerHTML = request.text;
       img.setAttribute('hidden','true');
-      content.innerHTML = '<p>' + url + '</p>';
+    }
   });
